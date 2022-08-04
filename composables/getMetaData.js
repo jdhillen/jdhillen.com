171867f288e96a6f399ec0cf0e835f7f0aa2b3df
Hillen, J.D <jd.hillen@garmin.com>
2022-08-04 15:53:57 -0500

    Testing Meta Desc and Image

--- composables/getMetaData.js
+++ composables/getMetaData.js
@@ -1,25 +1,26 @@
 export default function getMetaData(pageName, data) {
   const route = useRoute()
-  const meta = ref(data);
+  const description = ref(data[0].description);
+  const image = ref(data[0].image);
   return {
     title: `J.D. Hillen - ${pageName}`,
     htmlAttrs: {
       lang: 'en',
     },
     meta: [
-      { name: 'description', content: meta.value[0].description },
+      { name: 'description', content: description },
       { name: 'og:type', content: 'website' },
       { name: 'og:url', content: `https://jdhillen.com${route.fullPath}` },
       { name: 'og:title', content: `J.D. Hillen - ${pageName}` },
-      { name: 'og:description', content: meta.value[0].description },
-      { name: 'og:image', content: meta.value[0].image },
+      { name: 'og:description', content: description },
+      { name: 'og:image', content: image },
       { name: 'twitter:card', content: 'summary_large_image' },
       { name: 'twitter:site', content: '@jdhillen' },
       { name: 'twitter:creator', content: '@jdhillen' },
       { name: 'twitter:url', content: `https://jdhillen.com${route.fullPath}` },
       { name: 'twitter:title', content: `J.D. Hillen - ${pageName}` },
-      { name: 'twitter:description', content: meta.value[0].description },
-      { name: 'twitter:image', content: meta.value[0].image }
+      { name: 'twitter:description', content: description },
+      { name: 'twitter:image', content: image }
     ]
   }
 };
