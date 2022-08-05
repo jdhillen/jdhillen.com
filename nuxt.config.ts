1711c28a2bbd7d6c4cb0737807f88fbf9ac3e174
Hillen, J.D <jd.hillen@garmin.com>
2022-08-04 20:37:34 -0500

    Update - Meta Tags

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -25,19 +25,18 @@ export default defineNuxtConfig({
 
   meta: {
     meta: [
-      { charset: 'utf-8' },
-      { meta: 'viewport', content: 'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no'},
+      { name: 'viewport', content:'width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no' },
       { name: 'format-detection', content: 'telephone=no' },
-      { name: 'msapplication-TileColor', content: '#333333'},
-      { name: 'theme-color', content: '#FFFFFF'},
+      { name: 'msapplication-TileColor', content: '#333333' },
+      { name: 'theme-color', content: '#FFFFFF' },
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: '/apple-touch-icon.png'},
-      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png'},
-      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png'},
-      { rel: 'manifest', href: '/site.webmanifest'},
-      { rel: 'mask-icon', color: '#333333', href: '/safari-pinned-tab.svg'},
+      { rel: 'apple-touch-icon', type: 'image/png', sizes: '180x180', href: '/apple-touch-icon.png' },
+      { rel: 'icon', type: 'image/png', sizes: '32x32', href: '/favicon-32x32.png' },
+      { rel: 'icon', type: 'image/png', sizes: '16x16', href: '/favicon-16x16.png' },
+      { rel: 'manifest', href: '/site.webmanifest' },
+      { rel: 'mask-icon', color: '#333333', href: '/safari-pinned-tab.svg' },
       { rel: 'stylesheet', href: '//cdnjs.cloudflare.com/ajax/libs/highlight.js/11.6.0/styles/hybrid.min.css' }
     ],
     script: [
