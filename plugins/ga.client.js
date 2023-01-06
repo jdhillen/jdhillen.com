f94eeabc1b0a56b948fca050643827cf0706d831
Hillen, J.D <jd.hillen@garmin.com>
2023-01-06 11:01:55 -0600

    Update - removed routes

--- plugins/ga.client.js
+++ plugins/ga.client.js
@@ -1,13 +1,9 @@
 import VueGtag from 'vue-gtag';
 
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(
-    VueGtag,
-    {
-      property: {
-        id: 'UA-4521767-2'
-      }
-    },
-    nuxtApp.$router
-  );
+  nuxtApp.vueApp.use(VueGtag, {
+    property: {
+      id: 'UA-4521767-2'
+    }
+  });
 });
