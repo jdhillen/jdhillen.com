5032ca3750ac5d1d419d67e3e26816b87f4e0e5b
jdhillen <jhillen01@gmail.com>
2024-04-04 23:07:09 -0500

    Added - New RSS Feed

--- composables/usePageSetup.js
+++ composables/usePageSetup.js
@@ -4,9 +4,7 @@ const usePageSetup = async () => {
 
   const { data } = await useAsyncData('page', async () => {
     const { data, error } = await client.from('pages').select().eq('slug', route.name);
-
     if (error) throw createError({ statusCode: 404, statusMessage: 'Connection to database has been lost.' });
-
     return data[0];
   });
 
